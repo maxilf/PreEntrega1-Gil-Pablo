bb265e395888c52b6b2e474962c351ab1715db7d
maxilf <pablomg988@gmail.com>
2023-11-16 10:04:14 -0300

    Se agrega y da estilo al contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
+
   return (
+    <>
       <div className='App'>
         <NavBar/>
         <ItemListContainer greeting={"Bienvenidos"}/>
+        <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ', quantity)}/>
       </div>
-  )
+
+    </>
+  );
 }
 
 export default App;
